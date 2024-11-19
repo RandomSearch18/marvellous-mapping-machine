ad7e078a5d230e3b0d636199da41e8d77813d54e
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-11-19 21:20:22 +0000

    Add validate_file_is_readable(); + docstrings

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
+from pathlib import Path
 from sys import argv, stderr
 
 
-def print_error(message):
+def print_error(message: str):
+    """Prints an error message to stderr"""
     print(message, file=stderr)
 
 
 def validate_args():
+    """Returns True if the number of command-line arguments to the program is correct.
+    Otherwise, prints an appropriate error message and returns False."""
     if len(argv) == 2:
         return True
     if len(argv) < 2:
@@ -20,7 +24,25 @@ def validate_args():
 
 
 def get_data_file_path():
+    """Returns the path to the OSM data file that should be used by the routing engine"""
     return argv[1]
+
+
+def validate_file_is_readable(file_path: str):
+    """Returns True if the provided path points to an existing, readable, file-y file.
+    Otherwise, prints an appropriate error message and returns False."""
+    try:
+        with open(file_path, "r") as file:
+            if not Path(file.name).is_file():
+                print_error(f"Cannot access {file_path}: not a file")
+            return True
+
+    except FileNotFoundError:
+        print_error(f"File {file_path} not found.")
+        return False
+    except PermissionError:
+        print_error(f"Cannot access file {file_path}: permission denied")
+        return False
 
 
 if __name__ == "__main__":
