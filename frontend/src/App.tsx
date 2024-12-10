57baf68fa1cf02b1f51be8b1cfdcb74ea5c4b0a1
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-12-10 09:28:06 +0000

    Move the HTML into App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,47 @@
 function App() {
-    return <div>
-        AAAAAAAAAA
-    </div>
+  return (
+    <>
+      <h1 class="sr-only">Marvellous mapping machine</h1>
+      <div class="content">
+        <div id="screens">
+          <div class="screen" data-screen="map" id="active-screen">
+            <div id="main-map"></div>
+            <div class="fixed bottom-[6rem] right-2 z-[1000]">
+              <div
+                class="tooltip tooltip-left"
+                data-tip="Show current location"
+              >
+                <button
+                  class="btn btn-square btn-md btn-primary text-2xl"
+                  id="show-location"
+                >
+                  <span class="sr-only">Show current location</span>
+                  📍
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="screen" data-screen="route">
+            Route screen!
+          </div>
+          <div class="screen" data-screen="options">
+            Options screen!
+          </div>
+        </div>
+        <div class="btm-nav" id="bottom-bar">
+          <button class="active border-t-4 border-pink-800 bg-pink-200 text-pink-800">
+            <span class="btm-nav-label">Map</span>
+          </button>
+          <button class="bg-pink-100 text-pink-800">
+            <span class="btm-nav-label">Route</span>
+          </button>
+          <button class="bg-pink-100 text-pink-800">
+            <span class="btm-nav-label">Options</span>
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,14 @@
 import { render } from "voby"
 import App from "./App"
 import "leaflet/dist/leaflet.css"
-import "./bottomBar.mjs"
-import "./showCurrentLocation.mjs"
-import "./mainMap.mjs"
 
 const appElement = document.querySelector("#app")
 if (!appElement) {
   throw new Error("No app element found")
 }
 
-console.log("main.tsx loaded")
+render(<App />, appElement)
 
-// render(<App />, appElement)
+import("./bottomBar.mjs")
+import("./showCurrentLocation.mjs")
+import("./mainMap.mjs")
