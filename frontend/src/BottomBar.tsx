8589144c7de5819e7a7f4e2bb2c94958fe95c3f0
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-12-13 15:25:55 +0000

    BottomBar: Switch to observables

--- frontend/src/BottomBar.tsx
+++ frontend/src/BottomBar.tsx
@@ -1,52 +1,49 @@
-import { $, For } from "voby"
+import { $, For, useEffect } from "voby"
 
 function BottomBar() {
   function onClick(event: MouseEvent) {
+    // Get the button element that was clicked on
     const eventTarget = event.target as HTMLElement
     const button = eventTarget.closest("button")
-    const bottomBar = eventTarget.closest("#bottom-bar")
-    if (!button)
-      return console.warn("Handling bottom bar click event without a button")
-    if (!bottomBar)
-      return console.warn("Bottom button without a bottom bar was clicked")
-    // if (target.classList.contains("active")) return
-    const oldActiveButton = bottomBar.querySelector(".active")
-    if (oldActiveButton) {
-      oldActiveButton.classList.remove("active")
-      oldActiveButton.classList.remove("border-pink-600")
-      oldActiveButton.classList.remove("bg-pink-200")
-      oldActiveButton.classList.remove("border-t-4")
-      oldActiveButton.classList.add("bg-pink-100")
-    } else {
-      console.warn("No active button to deactivate")
+    if (!button) {
+      throw new Error("Couldn't find button that was clicked on")
     }
-    button.classList.add("active")
-    button.classList.add("border-pink-600")
-    button.classList.add("bg-pink-200")
-    button.classList.add("border-t-4")
-    button.classList.remove("bg-pink-100")
-
-    const activeScreen = document.querySelector("#active-screen")
-    const nextScreen = document.querySelector(
-      `[data-screen="${button.innerText.toLowerCase()}"]`
-    )
-    if (!nextScreen) {
-      throw new Error(`No screen found for ${button.innerText}`)
-    }
-    if (activeScreen) {
-      activeScreen.id = ""
-    } else {
-      console.warn("No active screen to deactivate")
-    }
-    nextScreen.id = "active-screen"
+    // Update the active screen to whichever screen our button corresponds to
+    const screenName = button.textContent!
+    activeScreen(screenName)
   }
 
   const bottomBarButtons = Object.fromEntries(
     ["Map", "Route", "Options"].map((name) => [name, $(false)])
   )
+  const activeScreen = $("Map")
+  useEffect(() => {
+    // This is a handler function for when the active screen changes
+    const newActiveScreen = activeScreen()
+
+    // Update the bottom bar buttons (specifically their active state)
+    for (const [name, button] of Object.entries(bottomBarButtons)) {
+      button(name === newActiveScreen)
+    }
+
+    // Make the new active screen visible and hide the old one
+    const oldScreenElement = document.querySelector("#active-screen")
+    const newScreenElement = document.querySelector(
+      `[data-screen="${newActiveScreen.toLowerCase()}"]`
+    )
+    if (!newScreenElement) {
+      throw new Error(`No screen found for ${newActiveScreen}`)
+    }
+    if (oldScreenElement) {
+      oldScreenElement.id = ""
+    } else {
+      console.warn("No active screen to deactivate")
+    }
+    newScreenElement.id = "active-screen"
+  })
 
   // Map is the default view
-  bottomBarButtons["Map"](true)
+  activeScreen("Map")
 
   return (
     <div class="btm-nav" id="bottom-bar" onClick={onClick}>
