40da4f96508c199644c597293fff40081af2f989
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-01-08 09:10:42 +0000

    only test routing engine when not running in JS

--- backend/main.py
+++ backend/main.py
@@ -73,17 +73,15 @@ if __name__ == "__main__":
     try:
         export_to_js_window()
     except ImportError:
-        pass
-
-    routing_engine = RoutingEngine()
-    ways, raw_nodes = routing_engine.download_osm_data(
-        BoundingBox(51.26268, -0.41497, 51.27914, -0.36755)
-    )
-    print("Computing routing graph")
-    routing_graph = routing_engine.compute_graph(ways, raw_nodes)
-    calculator = RouteCalculator(routing_graph, RoutingOptions())
-    print("Calculating route")
-    start = 51.273330, -0.397460
-    end = 51.274179, -0.391324
-    route = calculator.calculate_route_a_star(start, end)
-    print(route)
+        routing_engine = RoutingEngine()
+        ways, raw_nodes = routing_engine.download_osm_data(
+            BoundingBox(51.26268, -0.41497, 51.27914, -0.36755)
+        )
+        print("Computing routing graph")
+        routing_graph = routing_engine.compute_graph(ways, raw_nodes)
+        calculator = RouteCalculator(routing_graph, RoutingOptions())
+        print("Calculating route")
+        start = 51.273330, -0.397460
+        end = 51.274179, -0.391324
+        route = calculator.calculate_route_a_star(start, end)
+        print(route)
