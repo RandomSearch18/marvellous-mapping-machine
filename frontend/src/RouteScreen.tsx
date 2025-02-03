50c2428fad33bd23ae7f7dee76eb4b47a782e907
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-02-03 10:48:36 +0000

    testing throttling

--- frontend/src/RouteScreen.tsx
+++ frontend/src/RouteScreen.tsx
@@ -3,6 +3,7 @@ import { usePy } from "./pyscript.mts"
 import { currentRoute } from "./currentRoute.mts"
 import { BboxTuple, Coordinates, Line, NominatimPlace } from "./types.mts"
 import { ValidationError } from "./validationError.mts"
+import { throttle } from "dettle"
 
 enum CalculationState {
   Idle,
@@ -84,17 +85,29 @@ async function geocodeAddress(address: string): Promise<NominatimPlace | null> {
   return place
 }
 
+// async function displayResolvedAddress(inputId: string) {
+//   const input = document.getElementById(inputId)
+//   if (!(input instanceof HTMLInputElement))
+//     throw new Error(`Input element #${inputId} not found`)
+//   const address = input.value
+//   if (!address) return alert("No address provided")
+//   const place = await geocodeAddress(address)
+//   if (!place) return alert(`Couldn't find address: ${address}`)
+//   alert(
+//     `Address:\n${place.display_name}\n\nCoordinates: ${place.lat}, ${place.lon}`
+//   )
+// }
+
+const sendReq = () => {
+  console.log("Fire request!", new Date())
+  return 5
+}
+
+const sendReqThrottled = throttle(sendReq, 1000)
+
 async function displayResolvedAddress(inputId: string) {
-  const input = document.getElementById(inputId)
-  if (!(input instanceof HTMLInputElement))
-    throw new Error(`Input element #${inputId} not found`)
-  const address = input.value
-  if (!address) return alert("No address provided")
-  const place = await geocodeAddress(address)
-  if (!place) return alert(`Couldn't find address: ${address}`)
-  alert(
-    `Address:\n${place.display_name}\n\nCoordinates: ${place.lat}, ${place.lon}`
-  )
+  const res = sendReqThrottled()
+  console.log("Response:", res)
 }
 
 async function getCoordsFromInput(inputId: string): Promise<Coordinates> {
