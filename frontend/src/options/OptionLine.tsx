9084b0405ca0c9b632d9c1e1a347a43b66ae150c
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-02-12 23:32:42 +0000

    Fix styling of option lines

--- frontend/src/options/OptionLine.tsx
+++ frontend/src/options/OptionLine.tsx
@@ -7,8 +7,10 @@ function OptionLine({
 }) {
   return (
     <div class="px-2 first-of-type:border-t-[1px] border-b-[1px] border-solid border-[currentColor] hover:bg-pink-500 hover:bg-opacity-10">
-      <span class="label-text">{label}</span>
-      {input}
+      <div class="label">
+        <span class="label-text">{label}</span>
+        {input}
+      </div>
     </div>
   )
 }
