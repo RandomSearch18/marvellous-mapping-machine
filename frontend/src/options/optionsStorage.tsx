350e83134676d7222d889ac43f45aa0663db25ba
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-02-12 14:58:52 +0000

    Set default routing options in frontend (not working)

--- frontend/src/options/optionsStorage.tsx
+++ frontend/src/options/optionsStorage.tsx
@@ -1,4 +1,5 @@
 import { store, useEffect } from "voby"
+import { RoutingOptionsOptions, TriStateOption } from "../pyscript.mts"
 
 export type Options = {
   /** Frontend-only options */
@@ -6,12 +7,34 @@ export type Options = {
     weightOverlay: boolean
     bboxOverlay: boolean
   }
+  /** Options to be passed to the Python routing engine */
+  router: RoutingOptionsOptions
 }
 
 const defaultOptions: Options = {
   app: {
     weightOverlay: false,
     bboxOverlay: false,
+  },
+  router: {
+    unpaved_paths: TriStateOption.Neutral,
+    paved_paths: TriStateOption.Neutral,
+    covered_paths: TriStateOption.Prefer,
+    indoor_paths: TriStateOption.Neutral,
+    steps: TriStateOption.Neutral,
+    prefer_marked_crossings: false,
+    prefer_traffic_light_crossings: false,
+    prefer_dipped_kerbs: false,
+    prefer_tactile_paving: false,
+    allow_private_access: false,
+    allow_customer_access: true,
+    allow_walking_on_roads: true,
+    allow_higher_traffic_roads: true,
+    rights_of_way: TriStateOption.Prefer,
+    maintained_paths: TriStateOption.Prefer,
+    desire_paths: TriStateOption.Neutral,
+    treacherous_paths: TriStateOption.Avoid,
+    wheelchair_accessible: false,
   },
 }
 
