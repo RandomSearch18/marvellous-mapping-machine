3a601b6f444f4eb28d0910c25d56cff7aa0ee013
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-06-25 11:47:05 +0100

    Create a ReelsScreen component

---
+++ frontend/src/ReelsScreen.tsx
@@ -0,0 +1,19 @@
+function ReelsScreen() {
+  return (
+    <div class="mx-3">
+      <h2 class="font-bold text-4xl mt-5 mb-8">Reels</h2>
+      <p class="mb-8">
+        Bored of nature? Download reels here so that you can watch them offline.
+      </p>
+      <a
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        target="_blank"
+        class="btn btn-primary"
+      >
+        Browse Reels
+      </a>
+    </div>
+  )
+}
+
+export default ReelsScreen
