60b9984140802b5df4cab9cff47ab0bcb42cbc0b
MMK21 <50421330+MMK21Hub@users.noreply.github.com>
2026-02-12 14:54:22 +0000

    Fix some OptionLine styles

--- frontend/src/options/OptionLine.tsx
+++ frontend/src/options/OptionLine.tsx
@@ -6,8 +6,8 @@ function OptionLine({
   label: JSX.Child
 }) {
   return (
-    <div class="px-2 first-of-type:border-t border-b border-solid border-[currentColor] hover:bg-pink-500 hover:bg-opacity-10">
-      <div class="label">
+    <div class="px-2 first-of-type:border-t border-b border-solid border-[currentColor] hover:bg-pink-500/10">
+      <div class="label flex justify-between padding-y-[0.5rem] padding-x[0.25rem]">
         <span class="label-text dark:text-primary">{label}</span>
         {input}
       </div>
